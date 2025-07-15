5db5010ab250c36f35bc3e6719278c5363bdacd2
arevarapunagalakshmi@gmail.com <xxxxxxx@gmaicls>
2025-07-15 13:54:35 +0530

    Frontend

---
+++ Project2/script.js
@@ -0,0 +1,14 @@
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
+
+// Draw green rectangle (like your screenshot)
+ctx.fillStyle = 'green';
+ctx.fillRect(10, 10, 150, 100);
+
+
+
+// Draw red text
+ctx.fillStyle = 'red';
+ctx.font = '20px Arial';
+
+
